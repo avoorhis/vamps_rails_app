46e084fda32ba3f0e50956f1a91493ca96e67e48
annaship <tetya-loshad@yandex.ru>
2013-08-08 17:21:27 -0400

    EnvSampleSource model

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,8 +1,10 @@
 class Dataset < ActiveRecord::Base
   belongs_to :project
-  belongs_to :user
+  belongs_to :user #TODO: How?
+  belongs_to :env_sample_source
 
-  validates :dataset, :dataset_description, :presence => true, :format => /[A-Za-z1-9]/
+  validates :dataset, :dataset_description, :presence => true, :format => /[A-Za-z1-9 ,]/
+  validates :dataset, :dataset, :presence => true, :format => /[A-Za-z1-9_]/
   validates :project_id, :presence => true
 
 end
