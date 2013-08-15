4ecf2b07a01fab695efe960bd9eca143d300f636
annaship <tetya-loshad@yandex.ru>
2013-08-15 14:54:29 -0400

    seed, model: primer, project, rank, run_info

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,8 @@ class Project < ActiveRecord::Base
 	has_many   :datasets
   has_many   :run_infos, :through => :datasets
 
-	validates :project, :title, :project_description, :presence => true, :format => /[A-Za-z1-9]/
-	validates :contact_id, :env_sample_source_id, :presence => true	
+  validates :project, :title, :project_description, :presence => true, :format => /[A-Za-z1-9_]/
+	validates :contact_id, :presence => true	
 	validates :project, uniqueness: true
   validates :rev_project_name, uniqueness: true
   
--- app/models/rank.rb
+++ app/models/rank.rb
@@ -2,7 +2,7 @@ class Rank < ActiveRecord::Base
   belongs_to :sequence_uniq_info
   
   # TODO: Do we need all of them?
-  validates :rank, inclusion: { in: ["NA", "class", "domain", "family", "genus", "order", "orderx", "phylum", "species", "strain", "superkingdom"],
+  validates :rank, inclusion: { in: ["NA", "class", "domain", "family", "genus", "order", "orderx", "phylum", "species", "strain", "superkingdom"], \
       message: "%{value} is not a valid rank" }, uniqueness: true
   
 end
