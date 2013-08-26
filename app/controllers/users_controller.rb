894889010cb3d9335889146d9c11965443d84e4e
annaship <tetya-loshad@yandex.ru>
2013-08-26 12:55:21 -0400

    rspec tests start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
-
+  
+  def create
+    @user = current_user.build(params[:user])
+    @user.save
+  end
+  
+  # @users = User.includes(:contacts)
+  @contacts = @user.contacts
+  puts "URA1"
+  puts params[:post]
+  puts current_user
   private
   # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(:user, :email, :password, :password_confirmation)
-  end
+  # def user_params
+  #   a = params.require(:user)
+  #   puts a
+  #   # params.require(:user).permit(:user, :email, :password, :password_confirmation)
+  # end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,17 @@ module VampsApp6
     config.assets.enable = true
     config.active_record.schema_format setting = :sql # http://guides.rubyonrails.org/migrations.html#types-of-schema-dumps
     #config.action_view.JavaScript_expansions[:defaults] = %w(jquery rails application) 
+    
+    # from http://everydayrails.com/2012/03/12/testing-series-rspec-setup.html#sthash.OsHkyDKY.dpuf
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true
+        # ,
+        # :request_specs => true g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
