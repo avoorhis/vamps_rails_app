29a3669f9ed0fe2a0f07d32e0a5ac60962547b73
annaship <tetya-loshad@yandex.ru>
2013-09-10 15:24:16 -0400

    add taxon model

--- db/migrate/20130910192322_create_taxa.rb
+++ db/migrate/20130910192322_create_taxa.rb
@@ -1,6 +1,6 @@
 class CreateTaxa < ActiveRecord::Migration
   def change
-    create_table :taxas do |t|
+    create_table :taxa do |t|
       t.string :taxon, {:limit => 300, :null => false, :default => ''}
       t.integer :rank_id, :null => false      
       t.index  :rank, {:name => "rank", :unique => true}
--- spec/factories/taxa.rb
+++ spec/factories/taxa.rb
@@ -1,8 +1,6 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :taxa do
-    taxon ""
-    rank_id 1
+  factory :taxon do
   end
 end
