fc2a5e6e9684ecba653fb9c224d96cf3b3eaa72e
Andrew Voorhis <avoorhis@mbl.edu>
2013-09-12 14:05:10 -0400

    adjusted js dirs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-#//= require jquery
+//= require jquery
 //= require jquery_ujs
 //= require turbolinks
-#////= require_tree ./dhtmlx
-
-#//= require ./barchart
-#////= require ./dhtmlx_trees
+//= require ./barchart
 //= require_tree .
 
