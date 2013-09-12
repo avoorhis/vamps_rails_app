9567fa7f92d28d2df54f99a4fc3106ad02d248ba
Andrew Voorhis <avoorhis@mbl.edu>
2013-09-12 16:20:48 -0400

    add to tree js

--- app/assets/javascripts/visualization.js
+++ app/assets/javascripts/visualization.js
@@ -31,18 +31,54 @@ function toggleAll(name)
 
 //   	$(form).submit();
 // }
-function open_datasets(pid)
+function open_datasets(pid, project)
 {
   
+  //alert('in open')
   ds_div = document.getElementById(pid);
+  cbs = document.getElementsByName(project+'_ds_ids[]')
+  toggle = document.getElementById(project+'_toggle')
   if(ds_div.style.display == 'inline'){
+    // hide
     ds_div.style.display = 'none'
-
+    toggle.style.visibility = 'hidden'
+    // uncheck project
+    document.getElementById(project).checked = false
+    for(var i=0; i < cbs.length; i++) {
+      if(cbs[i].type == 'checkbox') {
+        cbs[i].checked=false        
+      }
+    }
   }else{
+    // show open dataset div
     ds_div.style.display = 'inline'
-
+    toggle.style.visibility = 'visible'
+    // check project
+    document.getElementById(project).checked = true
+    // now set all the ds checkboxes to 'checked'
+    for(var i=0; i < cbs.length; i++) {
+      if(cbs[i].type == 'checkbox') {
+        cbs[i].checked=true        
+      }
+    }
   }
   
+
+}
+
+function toggle_selected_datasets(project)
+{
   
+  cbs = document.getElementsByName(project+'_ds_ids[]')
+  ds_div = document.getElementById(pid);
+  for(var i=0; i < cbs.length; i++) {
+      if(ds_div.style.display == 'inline'){
+        cbs[i].checked=true        
+      }else{
+        cbs[i].checked=false
+      }
+    }
+
+ 
 
 }
