a35197ba151cc162a74b3f0510cdc7a9a111c01c
annaship <ashipunova@as-macbook.home>
2013-09-13 11:42:27 -0400

    create 3 diff projects

--- spec/factories/projects_factory.rb
+++ spec/factories/projects_factory.rb
@@ -4,10 +4,6 @@
 FactoryGirl.define do
   factory :project do |project|
     
-    # before(:create) do |project|
-    #   user = FactoryGirl.create(:user)
-    #   project.user_id = user.id
-    # end
     sequence(:project){|n| "SLM_NIH_v#{n}" }
 		project.title               "WWTP_Influents"
 		project.project_description "Analysis microbial communities found in sewage influent samples collected from a variety of WWTPs."
