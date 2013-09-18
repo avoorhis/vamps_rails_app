880a9f1a9e6aec2632e2589b3fb1118d76988d37
Andrew Voorhis <avoorhis@mbl.edu>
2013-09-18 15:05:13 -0400

    tax collector

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -15,6 +15,7 @@ class VisualizationController < ApplicationController
     we_have_some_data = false
     @master_sample_data = Hash.new
     @master_sample_data2 = []
+    
     temp_project = {}
     params.each do |k,v|
       
@@ -35,7 +36,9 @@ class VisualizationController < ApplicationController
           v = v.collect{|i| i.to_i}
           @master_sample_data[temp_project[:id]] = v
           myhash = {:pid=>temp_project[:id], :name=>temp_project[:name],  :datasets=>v,}
+          #myhash2 = {:pid=>temp_project[:id], :name=>temp_project[:name],  :datasets=>v,}
           @master_sample_data2.push(myhash)
+          #@master_sample_names.push(myhash2)
           temp_project = {}
         end
       end
@@ -58,8 +61,14 @@ class VisualizationController < ApplicationController
     domains      = params[:domains]
     @domains     = domains.compact
     # TODO: can we take a rank_id here, please?
-    @tax_rank    = params[:tax_rank] || 2
-    @this_rank = Rank.find_by_id(@tax_rank)
+    
+    rank_id = params[:tax_id]
+    puts 'this_rank id: '+rank_id.to_s
+    this_rank = Rank.find_by_id(rank_id)
+    @rank_number = this_rank.rank_number
+    @rank_name = this_rank.rank
+    puts 'this_rank num: '+@rank_number.to_s
+    puts 'this_rank name: '+@rank_name 
     # @rank_number = get_rank_num()
     @view        = params[:view]
     # params[:datasets] are created in visualization.js::getDatasets()
@@ -80,30 +89,23 @@ class VisualizationController < ApplicationController
     #     0;SLM_NIH_Bv4v5;1St_121_Stockton,
     #     0;SLM_NIH_Bv4v5;1St_120_Richmond,
     
-     @taxQuery    = create_tax_query()
-     print @taxQuery
-     @result      = Project.find_by_sql(@taxQuery)
-     taxonomy_by_site = {}    
+    @taxQuery    = create_tax_query()
+    print @taxQuery
+    result      = Project.find_by_sql(@taxQuery)
+    @taxonomy_hash = create_sorted_taxonomy_by_site(result)
+    puts "before fill with zeros:"
+    puts @taxonomy_hash
+    @taxonomy_by_site_hash = fill_in_zeros(@taxonomy_hash)
+    puts "after fill with zeros:"
+    puts @taxonomy_by_site_hash
+    # puts 'EXIT'
+    # @taxonomy_by_site_array = create_sorted_taxonomy_by_site(result)
+    # puts @taxonomy_by_site_array.inspect
+    # puts
+    # # change to array of hashes and fill in zeros
+    # @taxonomy_by_site_hash = fill_in_zeros(@taxonomy_by_site_array)
+    # puts @taxonomy_by_site_hash.inspect
 
-    for res in @result
-      pj  = res["project"]
-      ds  = res["dataset"]
-      pd = pj+'--'+ds
-      ts  = res["taxonomy"]
-      knt = res["knt"]
-      if taxonomy_by_site.has_key?(ts) then
-        # append
-        taxonomy_by_site[ts].merge!(pd => knt)
-      else
-        # add array to hash
-        taxonomy_by_site[ts] = {pd=>knt}
-      end        
-    end
-    # sort taxonomically alpha
-    puts taxonomy_by_site
-    @taxonomy_by_site = taxonomy_by_site.sort
-    # change to array of hashes and fill in zeros
-    #@taxonomy_by_site = fill_in_zeros(@taxonomy_by_site)
     #session[:taxonomy_by_site] = @taxonomy_by_site
 
   # # select SQL_CACHE project_dataset, taxon_string, knt, classifier, frequency, dataset_count FROM new_summed_data_cube  
@@ -111,11 +113,11 @@ class VisualizationController < ApplicationController
   #   # join new_taxon_string using(taxon_string_id, rank_number)  where
   #   # project_dataset in ('AB_SAND_Bv6--HS122','AB_SAND_Bv6--HS123') and  rank_number='1'
     
-    if params[:view] == "heatmap"
+    if params[:view]      == "heatmap"
       render :heatmap
-    elsif  params[:view] == "bar_charts"
+    elsif  params[:view]  == "bar_charts"
       render :bar_charts
-    else params[:view] == "tax_table"
+    else params[:view]    == "tax_table"
       #default
       render "tax_table"
     end    
@@ -157,23 +159,175 @@ class VisualizationController < ApplicationController
 ################################################################################
   private
 
-  
+def create_sorted_taxonomy_by_site(result)
+    taxonomy_hash =  {} 
+    @project_datasets = {}
+    for res in result
+      pj  = res["project"]
+      ds  = res["dataset"]
+      #pd = pj+'--'+ds
+      ts  = res["taxonomy"]
+      knt = res["knt"]
+      # create dataset names hash
+      # PD: {"SLM_NIH_Bv6"=>["SS_WWTP_1_25_11_2step"], "SLM_NIH_Bv4v5"=>["1St_121_Stockton", "1St_120_Richmond", "1St_152_Metro_North"]}
+      if @project_datasets.has_key?(pj) then
+        if ! @project_datasets[pj].include?(ds) 
+          @project_datasets[pj] << ds
+        end
+      else
+        @project_datasets[pj] = [ds]
+      end
+      if taxonomy_hash.has_key?(ts) then
+        if taxonomy_hash[ts].has_key?(pj) then
+          if taxonomy_hash[ts][pj].has_key?(ds) then
+            # sum knt for this ts, pj and ds
+            taxonomy_hash[ts][pj][ds] += knt 
+          else
+            #new ds
+            taxonomy_hash[ts][pj].merge!(ds=>knt) 
+          end          
 
+        else
+          # new pj
+          taxonomy_hash[ts].merge!(pj => {ds=>knt})
+        end
+      else
+        # new tax: add new hash if not already there
+        taxonomy_hash[ts] = {pj=>{ds=>knt}}
+      end    
+    end
+
+    return taxonomy_hash
+end
+#
+#  CREATE SORTED TAXONOMY BY SITE
+#
+# has all sites filled (with zeros if neccesary)
+# {
+#   "Bacteria;Bacteroidetes"=>
+#     {
+#       "SLM_NIH_Bv6"=>
+#         { 
+#           "SS_WWTP_1_25_11_2step"=>1011
+          
+#         },
+#       "SLM_NIH_Bv4v5"=>
+#         {
+#           "1St_121_Stockton"=>0, 
+#             "1St_120_Richmond"=>0, 
+#             "1St_152_Metro_North"=>0
+#         }
+#     },
+#   "Bacteria;Proteobacteria"=>{
+#     "SLM_NIH_Bv6"=>
+#       {
+#         "SS_WWTP_1_25_11_2step"=>1634236
+#       },
+#     "SLM_NIH_Bv4v5"=>
+#       {
+#         "1St_121_Stockton"=>11, 
+#         "1St_120_Richmond"=>4, 
+#         "1St_152_Metro_North"=>1
+#       }
+#   }
+# }
+  # def create_sorted_taxonomy_by_site(result)
+  #   taxonomy_by_site = {}  
+  #   @project_datasets = {}
+  #   for res in result
+  #     pj  = res["project"]
+  #     ds  = res["dataset"]
+  #     pd = pj+'--'+ds
+  #     ts  = res["taxonomy"]
+  #     knt = res["knt"]
+
+  #     # create dataset names hash
+  #     # PD: {"SLM_NIH_Bv6"=>["SS_WWTP_1_25_11_2step"], "SLM_NIH_Bv4v5"=>["1St_121_Stockton", "1St_120_Richmond", "1St_152_Metro_North"]}
+  #     if @project_datasets.has_key?(pj) then
+  #       if ! @project_datasets[pj].include?(ds) 
+  #         @project_datasets[pj] << ds
+  #       end
+  #     else
+  #       @project_datasets[pj] = [ds]
+  #     end
+  #     if taxonomy_by_site.has_key?(ts) then
+  #       if taxonomy_by_site[ts].has_key?(pd) then
+  #         # sum knt for this ts and ds
+  #         taxonomy_by_site[ts][pd] += knt 
+  #       else
+  #         # append new
+  #         taxonomy_by_site[ts].merge!(pd => knt)
+  #       end
+  #     else
+  #       # add new array to hash if not already there
+  #       taxonomy_by_site[ts] = {pd=>knt}
+  #     end        
+  #   end
+  #   # sort taxonomically alpha
+  #   #puts 'PD: '+@project_datasets.inspect
+  #   return taxonomy_by_site.sort
+  # end
+
+#
+#  FILL IN ZEROS
+#
   def fill_in_zeros(tax_hash)
-    new_tax_array = []
-    tax_hash.each do |tax, v|      
-      @datasets.each do |ds|
-        if not v.include?(ds) then
-          #tax_hash[tax].merge!(ds => 0)
-          v.merge!(ds => 0)
+    #new_tax_array = []
+    tax_hash.each do |tax, pj_hash| 
+      @project_datasets.each do |project,datasets| 
+        if not pj_hash.include?(project) then
+          # add the empty project
+          pj_hash.merge!(project => {})
         end
       end
-      new_tax_array.push({:taxonomy=>tax,:datasets=>v})
-    end
-    #return tax_hash
-    return new_tax_array
-  end
 
+      pj_hash.each do |p, ds_hash|
+        #puts 'ds_hash '+ds_hash.inspect
+        @project_datasets.each do |project,datasets| 
+          datasets.each do |dataset|
+            
+            #pd = project+'--'+dataset
+            if p==project and not ds_hash.include?(dataset) then
+            #tax_hash[tax].merge!(ds => 0)
+            ds_hash.merge!(dataset => 0)
+
+            end
+
+          end
+        end
+
+      end
+      #puts 'pj_hash '+pj_hash.inspect 
+      #new_tax_array.push({:taxonomy=>tax,:datasets=>v})
+    end
+    return tax_hash
+    #return new_tax_array
+  end
+  # def fill_in_zeros(tax_hash)
+  #   #new_tax_array = []
+  #   tax_hash.each do |tax, v|      
+  #     @project_datasets.each do |project,datasets| 
+  #       datasets.each do |dataset|
+
+  #         pd = project+'--'+dataset
+  #         if not v.include?(pd) then
+  #           #tax_hash[tax].merge!(ds => 0)
+  #           v.merge!(pd => 0)
+
+  #         end
+
+  #       end
+
+  #     end
+  #     #new_tax_array.push({:taxonomy=>tax,:datasets=>v})
+  #   end
+  #   return tax_hash
+  #   #return new_tax_array
+  # end
+
+#
+#  GET DATASET COUNTS
+#
   def get_dataset_counts()
     
     sql = "SELECT datasets.project_id, dataset_id, sum(seq_count) FROM sequence_pdr_infos
@@ -197,7 +351,10 @@ class VisualizationController < ApplicationController
     result = ActiveRecord::Base.connection.select_rows(sql)
     return result
   end
-    
+
+#
+#  CREATE COMMA LIST
+#    
   def create_comma_list(my_array)
     return "'" + my_array.join("', '") + "'"
   end
@@ -206,29 +363,25 @@ class VisualizationController < ApplicationController
     #rank_number = Rank.get_rank_number(@tax_rank)
     #puts 'rank id: '+rank_number  # == rank.id
   
-    puts @this_rank.rank_number 
+    
     #rank_id_names = %w[superkingdom_id phylum_id klass_id order_id family_id genus_id species_id strain_id]
     rank_names        = %w[superkingdom  phylum  klass   order  family   genus  species strain ]
     taxon_table_names = %w[superkingdoms phylums klasses orders families genera species strains]
     rank_ids = ""
     taxa_joins = ""
-    for n in 0..@this_rank.rank_number 
+    for n in 0..@rank_number 
       rank_ids += ' t'+(n+1).to_s+".#{rank_names[n]},"
       taxa_joins += "LEFT JOIN #{taxon_table_names[n]} AS t"+(n+1).to_s+" ON (#{rank_names[n]}_id = t"+(n+1).to_s+".id)\n"
     end
     
     return rank_ids[0..-2], taxa_joins
   end    
-  
+
+#
+#  CREATE TAX QUERY
+#
   def create_tax_query()
     
-    #projects_datasets = []
-    #sql_project_datasets     = @datasets.join("','")
-    # @datasets.each do |pd|
-    #   items = pd.split('--')
-    #   projects_datasets.push({:project=>items[0],:dataset=>items[1]})
-      
-    # end
 
     get_dataset_counts()
  
@@ -237,13 +390,13 @@ class VisualizationController < ApplicationController
     # TODO: get it from db
     #superkingdom  = Taxon.find_by_rank(@tax_rank)
     # will this always be '10'?
-    superkingdom_rank_id = 10
-    @superkingdom_list = Taxon.find_all_by_rank_id(superkingdom_rank_id)
+    
+    @superkingdom_list = Superkingdom.all
     # {"archaea"=>1,"bacteria"=>2, "organelle"=>3,"unknown"=>4,"eukarya"=>5}
 
     rank_ids, taxa_joins = make_taxa_by_rank()
     print "URA! rank_ids = #{rank_ids}\n taxa_joins = #{taxa_joins}"
-    taxQuery = "SELECT distinct projects.project, datasets.dataset, CONCAT_WS(\";\", #{rank_ids}) AS taxonomy, seq_count AS knt, classifier
+    taxQuery = "SELECT distinct projects.project as project, datasets.dataset as dataset, CONCAT_WS(\";\", #{rank_ids}) AS taxonomy, seq_count AS knt, classifier
 FROM sequence_pdr_infos"
 join = "\nJOIN datasets ON (dataset_id = datasets.id)
 JOIN projects ON (project_id = projects.id) 
@@ -270,7 +423,7 @@ JOIN taxonomies ON (taxonomies.id = taxonomy_id)
       
       sk_num = []
       @superkingdom_list.each do |sk|
-          if @domains.include? sk.taxon then
+          if @domains.include? sk.superkingdom then
             
               sk_num << sk.id.to_s
             
@@ -295,45 +448,10 @@ JOIN taxonomies ON (taxonomies.id = taxonomy_id)
         # and taxon_string not like '%_NA'
     end
 
-
-    #where    +=  " 
-    #AND sequence_uniq_infos.rank_id='#{@this_rank.rank_number}'  "
+    # there should be no GROUP BY or ORDER BY clause as it slows down the sql calls considerably
+    # but the seq_count has to be summed for each unique: project,dataset,taxonomy
+    group = "\nGROUP BY project, dataset, taxonomy"
    
-
-    # taxQuery = "SELECT project, dataset, taxon_string, knt, sdc.classifier, frequency, dataset_count
-    #               FROM summed_data_cube AS sdc"
-    # join     = "  JOIN projects on(project_id = projects.id),
-    #               JOIN datasets on(dataset_id = datasets.id)
-    #           "
-
-    # where    = "  WHERE project in ('#{sql_project}')
-    #               AND dataset in ('#{sql_dataset}')
-    #               AND rank_number='#{@rank_number}'"
-    # ##DOMAINS
-    # if @domains.length == 1 then
-    #   join  += "  JOIN taxonomies on(taxonomy_id = taxonomies.id)"
-    #   where += "  AND superkingdom_id = '#{superkingdom[@domains[0]]}'"
-    # elsif @domains.length == 5 then
-    #   # nothing extra here
-    # else 
-    #   sk_num = []
-    #   @domains.each do |d|
-    #     sk_num << superkingdom[d].to_s()
-    #   end
-    #   sql_superkingdom_ids     = sk_num.join("','")
-      
-    #   join  += " JOIN taxonomies using(taxon_string_id)"
-    #   where += " AND superkingdom_id in ('#{sql_superkingdom_ids}')"
-    # end
-    # ##NAs
-    # if @nas == 'no' then
-    #   # TODO
-    #   # and  taxon_string not like 'no_%' 
-    #     # and taxon_string not like 'NA%'
-    #     # and taxon_string not like '%;NA;%'
-    #     # and taxon_string not like '%;NA' 
-    #     # and taxon_string not like '%_NA'
-    # end
 
     @taxQuery = taxQuery + join + where
   end
