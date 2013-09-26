06ada5961aa331c8fb5ab4651d81dfe9a986b9e7
annaship <tetya-loshad@yandex.ru>
2013-09-26 15:36:44 -0400

    viz spec, intermediate

--- spec/requests/visualization_spec.rb
+++ spec/requests/visualization_spec.rb
@@ -3,12 +3,14 @@ require 'spec_helper'
 
 describe "Projects and Datasets list" do
   before(:each) do
+    Rank.delete_all
+    User.delete_all
+    Project.delete_all
     @user = FactoryGirl.create(:user)
     @user.confirm!    
     login_as(@user, :scope => :user)
 
     @projects = @user.projects
-    Rank.delete_all
     @ranks    = FactoryGirl.create(:rank)
     # @taxa     = Array.new(3) { FactoryGirl.build(:taxon) } 
             
@@ -22,7 +24,11 @@ describe "Projects and Datasets list" do
     page.html.should include('_Stockton')
   end
 
-  it "should check all underlying dataset checkboxes if its project was chosen" 
-  
+  it "should check all underlying dataset checkboxes if its project was chosen" do
+    project_name = Project.first.project
+    puts "URA: #{project_name}"
+    
+    check(project_name)
+  end
   it "should keep datasets checked after we hide the list"
 end
