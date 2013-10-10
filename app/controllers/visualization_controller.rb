6ee3683c6653a67e6d2ca1974ef6b877ed840828
annaship <tetya-loshad@yandex.ru>
2013-10-10 12:00:19 -0400

    dataset_not_choosen

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -7,27 +7,29 @@ class VisualizationController < ApplicationController
   
   def parse_view
     # TEST:    
-    @ordered_projects, @ordered_datasets = create_ordered_datasets() 
+    # @ordered_projects, @ordered_datasets = create_ordered_datasets() 
     @datasets_by_project_all = make_datasets_by_project_hash()
     # puts "@datasets_by_project_all = " + @datasets_by_project_all.inspect
+
+    unless (params.has_key?(:dataset_ids))
+      dataset_not_choosen()
+      return      
+    end
+
     d_ids = params[:dataset_ids] #TODO: take the ids from params[:dataset] and move to the main
     @datasets_per_pr = Dataset.all.find(d_ids) #TODO: move to the main
     
     p_ids = params[:project_ids] 
     p_o = @datasets_by_project_all.select {|p_o| p_o.attributes["id"] if p_ids.include? p_o.attributes["id"].to_s }
-    puts "p_o = " + p_o.inspect
-    puts "p_o.size = " + p_o.size.to_s
-    puts "datasets_by_project_all.size = " + @datasets_by_project_all.size.to_s
-    
-    puts "datasets_per_pr = " + @datasets_per_pr.inspect
-    puts 'ordered projects: ' +@ordered_projects.inspect
-    puts 'ordered datasets: ' +@ordered_datasets.inspect
+
+    # puts "p_o = " + p_o.inspect
+    # puts "p_o.size = " + p_o.size.to_s
+    # puts "datasets_by_project_all.size = " + @datasets_by_project_all.size.to_s
+    # 
+    # puts "datasets_per_pr = " + @datasets_per_pr.inspect
+    # puts 'ordered projects: ' +@ordered_projects.inspect
+    # puts 'ordered datasets: ' +@ordered_datasets.inspect
     #puts 'ordered datasets list: ' +@ds_id_list.inspect
-    if not @ordered_datasets.any?
-      flash.alert = 'Choose some data!'
-      redirect_to visualization_index_path
-      return
-    end
     #puts 'Which is a better format: '
     #puts '  this? a simple hash: ' + @master_sample_data.inspect
     #puts '  Or this? an array of hashes: '+@master_sample_data2.inspect
@@ -80,6 +82,10 @@ class VisualizationController < ApplicationController
     end    
   end
 
+  def dataset_not_choosen()
+    flash.alert = 'Choose some data!'
+    redirect_to visualization_index_path
+  end
   # def get_taxonomy_by_site
   #   return @taxonomy_by_site
   # end
