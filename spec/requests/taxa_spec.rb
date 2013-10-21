4859487f8ae9036c3eba1fb01e3de4bbb2d6072e
annaship <ashipunova@as-macbook.home>
2013-10-21 13:44:52 -0400

    taxa_spec: correct taxa per dataset counts, green

--- spec/requests/taxa_spec.rb
+++ spec/requests/taxa_spec.rb
@@ -17,6 +17,15 @@ describe "Taxa" do
     @taxonomies << FactoryGirl.create(:taxonomy, id: 137, domain_id: 2, phylum_id: 3, klass_id: 5, order_id: 65, family_id: 129, genus_id: 129, species_id: 129, strain_id: 4)
     # @taxonomies = Array.new(3) { FactoryGirl.create(:taxonomy) } 
 
+    @dat_counts_seq = [{:dataset_id=>3, :sequence_id=>1001, :seq_count=>2, :taxonomy_id=>96}, {:dataset_id=>3, :sequence_id=>1004, :seq_count=>8, :taxonomy_id=>82}, {:dataset_id=>3, :sequence_id=>1007, :seq_count=>3, :taxonomy_id=>137}, {:dataset_id=>4, :sequence_id=>1001, :seq_count=>2, :taxonomy_id=>96}, {:dataset_id=>4, :sequence_id=>1004, :seq_count=>4, :taxonomy_id=>82}]
+    puts "@dat_counts_seq = " + @dat_counts_seq.inspect
+    
+    @tax_hash_obj = TaxaCount.new
+    @tax_hash     = @tax_hash_obj.create(@taxonomies, @dat_counts_seq)
+    
+    puts "HERE0: @tax_hash_obj = " +  @tax_hash_obj.inspect
+    puts "@tax_hash_obj.taxa_count_per_d = " + @tax_hash_obj.taxa_count_per_d.inspect
+    
 
     visit "/visualization"      
   end
@@ -27,50 +36,22 @@ describe "Taxa" do
   end
   
   it "should create correct counts per dataset" do
-    @dat_counts_seq = [{:dataset_id=>3, :sequence_id=>1001, :seq_count=>2, :taxonomy_id=>96}, {:dataset_id=>3, :sequence_id=>1004, :seq_count=>8, :taxonomy_id=>82}, {:dataset_id=>3, :sequence_id=>1007, :seq_count=>3, :taxonomy_id=>137}, {:dataset_id=>4, :sequence_id=>1001, :seq_count=>2, :taxonomy_id=>96}, {:dataset_id=>4, :sequence_id=>1004, :seq_count=>4, :taxonomy_id=>82}]
-    puts "@dat_counts_seq = " + @dat_counts_seq.inspect
+    puts "HERE1: @tax_hash_obj = " +  @tax_hash_obj.inspect
     
-    tax_hash_obj = TaxaCount.new
-    tax_hash     = tax_hash_obj.create(@taxonomies, @dat_counts_seq)
-    puts "HERE0: tax_hash_obj = " +  tax_hash_obj.inspect
-    puts "tax_hash_obj.taxa_count_per_d = " + tax_hash_obj.taxa_count_per_d.inspect
-    tax_hash_obj.taxa_count_per_d.should == {2=>{3=>{3=>{16=>{18=>{129=>{129=>{4=>{:datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, 5=>{65=>{129=>{129=>{129=>{4=>{:datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>11, 4=>4}}, 4=>{32=>{5=>{52=>{76=>{129=>{4=>{:datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>13, 4=>6}}}
-    
+    @tax_hash_obj.taxa_count_per_d.should == {2=>{3=>{3=>{16=>{18=>{129=>{129=>{4=>{:datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, :datasets_ids=>{3=>8, 4=>4}}, 5=>{65=>{129=>{129=>{129=>{4=>{:datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>3}}, :datasets_ids=>{3=>11, 4=>4}}, 4=>{32=>{5=>{52=>{76=>{129=>{4=>{:datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>2, 4=>2}}, :datasets_ids=>{3=>13, 4=>6}}}
 
   end
   
-  # it "should give correct taxa per dataset counts" do
-  #   puts "Ura2"
-  #   puts @taxonomies.inspect
-  #   puts "@dat_counts_seq = " + @dat_counts_seq.inspect
-  #           
-  #   
-  #   tax_hash_obj = TaxaCount.new
-  #   tax_hash     = tax_hash_obj.create(@taxonomies, @dat_counts_seq)
-  #   puts "\nRES: tax_hash = " + tax_hash.inspect
-  #   a = tax_hash_obj.get_cnts_per_dataset_ids_by_tax_ids(tax_hash, [2]) #{3=>13, 4=>6}
-  #   puts "[2], get_cnts_per_dataset_ids_by_tax_ids = " + a.inspect
-  #   a = tax_hash_obj.get_cnts_per_dataset_ids_by_tax_ids(tax_hash, [2, 3]) #{3=>11, 4=>4}
-  #   puts "[2, 3], get_cnts_per_dataset_ids_by_tax_ids = " + a.inspect
-  #   a = tax_hash_obj.get_cnts_per_dataset_ids_by_tax_ids(tax_hash, [2, 3, 3, 16, 18, 129, 129, 4]) #=>{3=>8, 4=>4}
-  #   puts "[2, 3, 3, 16, 18, 129, 1, 4], get_cnts_per_dataset_ids_by_tax_ids = " + a.inspect
-  #   puts "HERE: tax_hash_obj = " +  tax_hash_obj.inspect
-  #   
-  # end
+  it "should give correct taxa per dataset counts" do
+    a = @tax_hash_obj.get_cnts_per_dataset_ids_by_tax_ids(@tax_hash, [2]) #{3=>13, 4=>6}
+    a.should == {3=>13, 4=>6}
 
-  
-  # it "should have projects and datasets" do
-  #   puts page.body
-  #   # puts "URA"
-  #   # puts FactoryGirl.attributes_for(:taxon)
-  #   # puts FactoryGirl.attributes_for(:rank)
-  #   # puts FactoryGirl.attributes_for(:user)
-  #   page.should have_content("SLM_NIH_v")
-  #   
-  #   page.html.should include('_Stockton')
-  # end
-  # 
-  # it "should check all underlying dataset checkboxes if its project was chosen" 
-  # 
-  # it "should keep datasets checked after we hide the list"
+    a = @tax_hash_obj.get_cnts_per_dataset_ids_by_tax_ids(@tax_hash, [2, 3]) #{3=>11, 4=>4}
+    a.should == {3=>11, 4=>4}
+
+    a = @tax_hash_obj.get_cnts_per_dataset_ids_by_tax_ids(@tax_hash, [2, 3, 3, 16, 18, 129, 129, 4]) #=>{3=>8, 4=>4}
+    a.should == {3=>8, 4=>4}
+    
+  end
+
 end
