6f699ed78da72b7ba44b72cf8647d914709ce1bb
annaship <ashipunova@as-macbook.home>
2013-10-21 18:37:25 -0400

    make taxon strings intermediate

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -28,7 +28,30 @@ class VisualizationController < ApplicationController
     @dat_counts_seq_tax    = {}    
     @taxonomy_w_cnts_by_d  = get_taxonomy_per_d(my_pdrs)
     
-    make_taxonomy_id_strings_by_rank()
+    taxonomy_id_strings_upto_rank = make_taxonomy_id_strings_upto_rank()
+    rank_names = get_rank_names()
+    rank_number = @rank_obj.rank_number
+    
+    ranks_to_use = rank_names[0..rank_number]
+    
+    all_taxa = get_all_taxa_from_db(ranks_to_use)
+    puts "AAA: all_taxa = " + all_taxa.inspect
+    taxonomy_id_strings_upto_rank.each do |k, v|
+      puts "k = #{k.inspect}, v = #{v.inspect}"
+      # k = 82, v = [2, 3, 3]
+      ranks_to_use.each do |r|
+        res   = all_taxa[r].select{|t| t.id == tax_id_val}  
+        use order name
+        puts "r = #{r.inspect}"
+        # r = "domain"
+        # r = "phylum"
+        # r = "klass"
+      end
+      
+    end
+    # res   = all_taxa[rank_name].select{|t| t.id == tax_id_val}  
+    
+    
     # puts "TTT: @taxonomy_w_cnts_by_d = " + @taxonomy_w_cnts_by_d.inspect
     
     if params[:view]      == "heatmap"
@@ -110,12 +133,12 @@ class VisualizationController < ApplicationController
   end
   
   def get_all_taxa_from_db(rank_names)
-    puts "rank_names = " + rank_names.inspect
+    # puts "rank_names = " + rank_names.inspect
     all_taxa   = Hash.new{|hash, key| hash[key] = []}
     rank_names.each do |rank_name|
       all_taxa[rank_name] = rank_name.camelize.constantize.all
     end
-    puts "all_taxa = " + all_taxa.inspect
+    # puts "all_taxa = " + all_taxa.inspect
     # {"domain"=>#<ActiveRecord::Relation [#<Domain id: 1, domain: "Archaea">, #<Domain id: 2, domain: "Bacteria">, #<Domain id: ...
     return all_taxa
   end  
@@ -172,14 +195,28 @@ class VisualizationController < ApplicationController
     # TODO:
     # RESULT for rank = klass:  {3=>["Bacteria;Proteobacteria;Gammaproteobacteria", "Bacteria;Actinobacteria;class_NA", "Bacteria;Proteobacteria;Alphaproteobacteria"], 4=>["Bacteria;Proteobacteria;Gammaproteobacteria", "Bacteria;Actinobacteria;class_NA"]}
     # 1) make taxonomy_id strings from Taxonomy by rank
-    taxonomy_id_strings_by_rank = make_taxonomy_id_strings_by_rank()
+    # taxonomy_id_strings_upto_rank = make_taxonomy_id_strings_upto_rank()
+    # rank_names = get_rank_names()
+    # rank_number = @rank_obj.rank_number
+    # 
+    # ranks_to_use = rank_names[0..rank_number]
+    # 
+    # all_taxa = get_all_taxa_from_db(ranks_to_use)
+    # puts "AAA: all_taxa = " + all_taxa.inspect
+    # taxonomy_id_strings_upto_rank.each do |taxonomy_id, taxon_ids_arr|
+    #   
+    #   taxon_ids_arr
+    #   # 82=>[2, 3, 3]
+    #   res   = all_taxa[rank_name].select{|t| t.id == tax_id_val}  
+    # end
+    
     # 2) get taxon names
     # 3) arrange by dataset_ids
     # 4) add counts to show in tax_table_view
     
   end
   
-  def make_taxonomy_id_strings_by_rank()
+  def make_taxonomy_id_strings_upto_rank()
     # from Taxonomy 
     # TODO: refactoring
     
@@ -192,12 +229,12 @@ class VisualizationController < ApplicationController
     rank_id_names = ranks_to_use.map{|rank_name| rank_name + "_id" }
     puts "MMM2: rank_id_names = " + rank_id_names.inspect
     
-    taxonomy_id_strings_by_rank = Hash.new{|hash, key| hash[key] = []}
-    @taxonomies.map{|t| tax_ids = []; t.attributes.select{|a| tax_ids << t[a] if rank_id_names.include? a }; taxonomy_id_strings_by_rank[t[:id]] = tax_ids}
-    puts "EEE: taxonomy_id_strings_by_rank = " + taxonomy_id_strings_by_rank.inspect
-    # EEE: taxonomy_id_strings_by_rank = {82=>[2, 3, 3], 96=>[2, 4, 32], 137=>[2, 3, 5]}
+    taxonomy_id_strings_upto_rank = Hash.new{|hash, key| hash[key] = []}
+    @taxonomies.map{|t| tax_ids = []; t.attributes.select{|a| tax_ids << t[a] if rank_id_names.include? a }; taxonomy_id_strings_upto_rank[t[:id]] = tax_ids}
+    puts "EEE: taxonomy_id_strings_upto_rank = " + taxonomy_id_strings_upto_rank.inspect
+    # EEE: taxonomy_id_strings_upto_rank = {82=>[2, 3, 3], 96=>[2, 4, 32], 137=>[2, 3, 5]}
     
-    return taxonomy_id_strings_by_rank
+    return taxonomy_id_strings_upto_rank
     # res = @taxonomies.map{|t| puts t.attributes}
     # {"id"=>82, "domain_id"=>2, "phylum_id"=>3, "klass_id"=>3, "order_id"=>16, "family_id"=>18, "genus_id"=>129, "species_id"=>129, "strain_id"=>4, "created_at"=>Mon, 19 Aug 2013 08:44:13 EDT -04:00, "updated_at"=>Mon, 19 Aug 2013 08:44:13 EDT -04:00}
     
@@ -205,7 +242,7 @@ class VisualizationController < ApplicationController
     # LLL1: res = [82, 96, 137]
     
     # .each do |taxonomy|
-    #   taxonomy_id_strings_by_rank  = []
+    #   taxonomy_id_strings_upto_rank  = []
     #   # puts "from loop: taxonomy = " + taxonomy.inspect 
     #   # puts "*" * 10
     #   # res   = taxonomy.select{|t| t.id == tax_id_val}  
