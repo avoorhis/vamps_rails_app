f491f0aba8eec95f654af58a5ca01c552db3e761
annaship <tetya-loshad@yandex.ru>
2013-10-22 17:13:40 -0400

    organize_tax_by_d_id - uniq result, works

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -235,19 +235,22 @@ class VisualizationController < ApplicationController
   def organize_tax_by_d_id(taxon_strings_upto_rank) 
     taxon_strings_per_d = Hash.new{|hash, key| hash[key] = []}
     taxon_strings_upto_rank.each do |tax_id, taxon_str_arr|
-      puts "UUU0: tax_id = " + tax_id.inspect
-      puts "UUU01: taxon_str_arr = " + taxon_str_arr.inspect
+      # puts "UUU0: tax_id = " + tax_id.inspect
+      # puts "UUU01: taxon_str_arr = " + taxon_str_arr.inspect
       @dat_counts_seq_tax.each do |dat_counts_seq_tax_hash|
         # a = dat_counts_seq_tax_hash[0].select {|h| h[:dataset_id] if h[:taxonomy_id].to_s == tax_id.to_s }
-        puts "UUU1: dat_counts_seq_tax_hash = " + dat_counts_seq_tax_hash.inspect
-        puts "UUU11: dat_counts_seq_tax_hash[:taxonomy_id] = " + dat_counts_seq_tax_hash[:taxonomy_id].inspect
+        # puts "UUU1: dat_counts_seq_tax_hash = " + dat_counts_seq_tax_hash.inspect
+        # puts "UUU11: dat_counts_seq_tax_hash[:taxonomy_id] = " + dat_counts_seq_tax_hash[:taxonomy_id].inspect
         # UUU1: dat_counts_seq_tax_hash = {:dataset_id=>3, :sequence_id=>1001, :seq_count=>2, :taxonomy_id=>96}
         taxon_strings_per_d[dat_counts_seq_tax_hash[:dataset_id]] << taxon_str_arr.uniq
+        # puts "UUU12: taxon_strings_per_d = " + taxon_strings_per_d.inspect
+        
       end
-      puts "UUU2: taxon_strings_per_d = " + taxon_strings_per_d.inspect
+      # puts "UUU2: taxon_strings_per_d = " + taxon_strings_per_d.inspect
     end
   # YYY: taxon_strings_upto_rank = {82=>["Bacteria", "Proteobacteria", "Gammaproteobacteria"], 96=>["Bacteria", "Actinobacteria", "class_NA"], 137=>["Bacteria", "Proteobacteria", "Alphaproteobacteria"]}
   # URA3 = @dat_counts_seq_tax: [{:dataset_id=>3, :sequence_id=>1001, :seq_count=>2, :taxonomy_id=>96}, {:dataset_id=>3, :sequence_id=>1002, :seq_count=>103, :taxonomy_id=>214}, {:dataset_id=>3, :sequence_id=>1004, :seq_count=>8, :taxonomy_id=>82}, {:dataset_id=>3, :sequence_id=>1005, :seq_count=>203, :taxonomy_id=>214}, {:dataset_id=>3, :sequence_id=>1007, :seq_count=>3, :taxonomy_id=>137}, {:dataset_id=>4, :sequence_id=>1001, :seq_count=>2, :taxonomy_id=>96}, {:dataset_id=>4, :sequence_id=>1002, :seq_count=>13, :taxonomy_id=>214}, {:dataset_id=>4, :sequence_id=>1004, :seq_count=>4, :taxonomy_id=>82}, {:dataset_id=>4, :sequence_id=>1005, :seq_count=>20, :taxonomy_id=>214}]
+    taxon_strings_per_d.map{|id, val_arr| taxon_strings_per_d[id] = val_arr.uniq}
     return taxon_strings_per_d
   end
   
