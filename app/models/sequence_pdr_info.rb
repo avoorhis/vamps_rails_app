370623193f15715d1f8ccb567d2f17782af34136
annaship <ashipunova@as-macbook.home>
2013-10-26 08:36:22 -0400

    pdr info model add uniq validation

--- app/models/sequence_pdr_info.rb
+++ app/models/sequence_pdr_info.rb
@@ -7,6 +7,7 @@ class SequencePdrInfo < ActiveRecord::Base
   
   validates :seq_count, numericality: { only_integer: true }
   validates :classifier, :inclusion => { :in => ['RDP', 'GAST'] }
+  validates_uniqueness_of :sequence_id, :scope => :dataset_id
   
   scope :taxonomy_ids, -> {includes(:sequence_uniq_info).select("sequence_uniq_infos.taxonomy_id").references(:sequence_uniq_infos) }
   
