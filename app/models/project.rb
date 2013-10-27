7c77905f899bf6d33c06c979b964ddecf42a0f87
annaship <ashipunova@as-macbook.home>
2013-10-27 11:44:19 -0400

    change validation to \A\z

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
 
   accepts_nested_attributes_for :datasets
 
-  validates :project, :title, :project_description, :presence => true, :format => /[A-Za-z1-9_]/
+  validates :project, :title, :project_description, :presence => true, :format => /\A[A-Za-z1-9_]+\z/
   validates :user_id, :presence => true 
   validates :project, uniqueness: true
   validates :rev_project_name, uniqueness: true
